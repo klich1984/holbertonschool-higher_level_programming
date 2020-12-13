3f3764f2910419b66f4f35da82b5ea2da5535360
klich1984 <2239@holbertonschool.com>
2020-12-13 17:46:52 +0000

    00 update badly copied string

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = str(number)[-1]
 if int(last_digit) > 5:
-	print("Last digit of", number, "is", last_digit, "and is greater than 5")
+    print("Last digit of", number, "is", last_digit, "and is greater than 5")
 elif int(last_digit) == 0:
     print("Last digit of", number, "is", last_digit, "and is 0")
 elif 0 < int(last_digit) < 6:
-    print("Last digit of", number, "is", last_digit, "and is less than 6 and not 0")
+    print("Last digit of", number, "is", last_digit, "and is less
+            than 6 and not 0")
