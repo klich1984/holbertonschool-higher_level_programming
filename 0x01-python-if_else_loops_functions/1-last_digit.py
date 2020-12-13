8e69723c25accb2ca6d0b0e332bbd78382ba748b
klich1984 <2239@holbertonschool.com>
2020-12-13 20:07:44 +0000

    06 update correcting number negatives runing

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,17 +5,16 @@ l_dig = str(number)[-1]
 f_dig = str(number)[0]
 n_neg = f_dig + l_dig
 
-if int(l_dig) > 5:
-    if number < 0:
-        print("Last digit of", number, "is", n_neg, "and is less than 6 and not 0")
-    else:
-        print("Last digit of", number, "is",l_dig, "and is greater than 5")
+if int(l_dig) > 5 and number > 0:
+    print("Last digit of", number, "is",l_dig, "and is greater than 5")
 
 elif int(l_dig) == 0:
     print("Last digit of", number, "is", l_dig, "and is 0")
 
-elif 0 < int(l_dig) < 6:
-    if number < 0:
-        print("Last digit of", number, "is", n_neg, "and is less than 6 and not 0")
-    else:
-        print("Last digit of", number, "is", l_dig, "and is less than 6 and not 0")
+elif int(l_dig) < 6 and number > 0:
+    print("Last digit of", number, "is", l_dig, "and is less than 6 and not 0")
+
+else:
+    print("Last digit of", number, "is", n_neg, "and is less than 6 and not 0")
+
+print(number)
