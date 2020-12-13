3ff6d0b3adbb12dddf28b973163dc7428d1cd902
klich1984 <2239@holbertonschool.com>
2020-12-13 22:07:39 +0000

    10 update resolve check red 5

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,13 +6,15 @@ f_dig = str(number)[0]
 n_neg = f_dig + l_dig
 
 if int(l_dig) > 5 and number > 0:
-    print("Last digit of", number, "is", l_dig, "and is greater than 5")
+    print("Last digit of", number, "is", int(l_dig), "and is greater than 5")
 
 elif int(l_dig) == 0:
-    print("Last digit of", number, "is", l_dig, "and is 0")
+    print("Last digit of", number, "is", int(l_dig), "and is 0")
 
 elif int(l_dig) < 6 and number > 0:
-    print("Last digit of", number, "is", l_dig, "and is less than 6 and not 0")
+    print("Last digit of", number, "is", int(l_dig),
+          "and is less than 6 and not 0")
 
 else:
-    print("Last digit of", number, "is", n_neg, "and is less than 6 and not 0")
+    print("Last digit of", number, "is", int(n_neg),
+          "and is less than 6 and not 0")
