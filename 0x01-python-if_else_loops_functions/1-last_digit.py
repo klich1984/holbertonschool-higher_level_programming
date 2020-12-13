1839d5b817a3238e14b33e16e5816adf91d7bd18
klich1984 <2239@holbertonschool.com>
2020-12-13 17:50:16 +0000

    03 update correcting bad git up

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,4 +7,5 @@ if int(last_digit) > 5:
 elif int(last_digit) == 0:
     print("Last digit of", number, "is", last_digit, "and is 0")
 elif 0 < int(last_digit) < 6:
+    print(number[0])
     print("Last digit of", number, "is", last_digit, "and is less than 6 and not 0")
