2aaab465200deb8815d2ceb911bc23fbae2cf9ac
klich1984 <2239@holbertonschool.com>
2020-12-15 23:49:04 +0000

    00 | add file | runing 6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix)-1:
+        for f in range(len(matrix)):
+            for c in range(len(matrix)-1):
+                print("{:d} ".format(matrix[f][c]), end="")
+            print("{:d}".format(matrix[f][c+1]))
+    else:
+        print()
