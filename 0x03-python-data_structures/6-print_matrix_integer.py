b0d7a6ebcb30374a4d30807f81097d6b091fec42
klich1984 <2239@holbertonschool.com>
2020-12-16 00:57:09 +0000

    01 | update | correcting matrix length

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     if len(matrix)-1:
         for f in range(len(matrix)):
-            for c in range(len(matrix)-1):
+            for c in range(len(matrix[f])-1):
                 print("{:d} ".format(matrix[f][c]), end="")
             print("{:d}".format(matrix[f][c+1]))
     else:
