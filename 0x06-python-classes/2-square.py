77ae2f2e50be1c25e3afa043973b65a9925616e5
klich1984 <2239@holbertonschool.com>
2021-01-05 21:31:07 -0500

    00 | add file | code running output good

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Create class Square"""
+
+
+class Square:
+    """class that calculates the square with the size"""
+    __size = 0
+
+    def __init__(self, size=None):
+        """Initializacion of the class Square
+
+        Args:
+            size is a int and is the size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = size
