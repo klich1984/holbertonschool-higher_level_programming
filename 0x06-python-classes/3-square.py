0ee690c83d98e8577bb7633acc31c213a7654c33
klich1984 <2239@holbertonschool.com>
2021-01-05 21:46:58 -0500

    00 | add file | code running output good

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Create class Square"""
+
+
+class Square:
+    """class that calculates the square with the size"""
+    __size = 0
+
+    def __init__(self, size=0):
+        """Initializacion of the class Square
+
+        Args:
+            size is a int and is the size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Calculate the area
+
+        Returns: that returns the current square area
+        """
+        return self.__size * self.__size
