f7cc36765b459c71a2fb2d4986ab33796bfc631e
klich1984 <2239@holbertonschool.com>
2021-01-10 20:01:32 -0500

    00 | add file | code running sums two integer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""sums two integers"""
+
+
+def add_integer(a, b=98):
+    """[summary]
+
+    Args:
+        a (int, float): Integer or float
+        b (int, float): Integer or float. Defaults to 98.
+
+    Returns: the sum of two integers provided they are of type integer or float
+    otherwise an exception with an error
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    elif type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
