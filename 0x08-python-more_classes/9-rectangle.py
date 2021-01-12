adfe16a7c2ff60060862317027b0ebdf3b68bd89
klich1984 <2239@holbertonschool.com>
2021-01-11 21:58:53 -0500

    02 | code fix | code running output good

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -139,11 +139,11 @@ class Rectangle:
         else:
             return rect_2
 
-
+    @classmethod
     def square(cls, size=0):
         """method that returns a new Rectangle instance
 
         Args:
             size (int): size of the rectangle. Defaults to 0.
         """
-        return cls(size * size)
+        return cls(size, size)
