43f11763c18bb3cfa865e24c6c407e15c626d5b7
klich1984 <2239@holbertonschool.com>
2021-01-20 18:24:15 -0500

    00 | code update | add end= in print

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -12,4 +12,4 @@ def read_file(filename=""):
     print(filename)
     with open(filename, 'r', encoding='utf-8') as f:
         read_file = f.read()
-    print(read_file)
+    print(read_file, end="")
