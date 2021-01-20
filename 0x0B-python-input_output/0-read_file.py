de61cb3af6c9e8cfc31fe1352f8a7d00fde150f0
klich1984 <2239@holbertonschool.com>
2021-01-20 15:13:24 -0500

    00 | add file | code running and output good

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Function that reads a text file (UTF8)
+and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """method that open, read and print a file (UTF8)
+
+    Args:
+        filename (str): File to read. Defaults to "".
+    """
+    print(filename)
+    with open(filename, 'r', encoding='utf-8') as f:
+        read_file = f.read()
+    print(read_file)
