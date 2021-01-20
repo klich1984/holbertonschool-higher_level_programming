e4c1a11160abdb5a505d1ea92be64afad71575d6
klich1984 <2239@holbertonschool.com>
2021-01-20 18:50:33 -0500

    00 | add file | code running and output good

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+"""function that appends a string at the end of
+    a text file (UTF8) and returns the number of
+    characters added
+"""
+
+
+def append_write(filename="", text=""):
+    """method that appends a string at the end of a text
+
+    Args:
+        filename (str): file to read and appends a string. Defaults to "".
+        text (str): string for appends an file. Defaults to "".
+    Return: returns the number of characters added
+    """
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
