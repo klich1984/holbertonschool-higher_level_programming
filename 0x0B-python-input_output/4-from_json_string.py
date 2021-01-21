dbfcfb9965401b1044ec60bc7268c7c938819145
klich1984 <2239@holbertonschool.com>
2021-01-20 20:17:26 -0500

    00 | add file | code running and outpu good

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""function that returns an object (Python data structure)
+    represented by a JSON string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """method that return an objet (python data struct)
+
+    Args:
+        my_str (str): file that convert a json python
+    """
+    #with open(my_str, 'r', encoding='utf-8') as f:
+    js = json.loads(my_str)
+    return js
