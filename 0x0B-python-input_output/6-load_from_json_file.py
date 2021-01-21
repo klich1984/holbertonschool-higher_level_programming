842d353c98dc65283f3b4ce2b2f62e693ef4739c
klich1984 <2239@holbertonschool.com>
2021-01-20 22:06:16 -0500

    00 | add file | code running and output good

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""function that creates an Object from a “JSON file”"""
+import json
+
+
+def load_from_json_file(filename):
+    """method that create object from a JSON file
+
+    Args:
+        filename ([str]): file where load the json
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        js = json.load(f)
+        return js
