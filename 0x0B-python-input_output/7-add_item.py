1c415bb446ff0248c7b502cfd02f2ddb293b8d95
klich1984 <2239@holbertonschool.com>
2021-01-20 23:05:59 -0500

    01 | add file | test create and save file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
+"""script that adds all arguments to a Python list,
+and then save them to a file
+"""
 import sys
+import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+name_file = "/home/klich1984/holbeton/holbertonschool-higher_level_programming/0x0B-python-input_output/add_item.json"
+try:
+    open(name_file, 'r', encoding='utf-8')
+    print("entre al try")
+except:
+    save_to_json_file(sys.argv[1:], name_file)
+else:
+    print("entre al else")
 print(sys.argv[1:])
