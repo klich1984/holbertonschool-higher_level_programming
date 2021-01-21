3eacd34ab5c52317edce3a43e9c526fd464ce584
klich1984 <2239@holbertonschool.com>
2021-01-21 00:00:52 -0500

    01 | code update | erase 'r'

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -9,6 +9,6 @@ def load_from_json_file(filename):
     Args:
         filename ([str]): file where load the json
     """
-    with open(filename, 'r', encoding='utf-8') as f:
+    with open(filename, encoding='utf-8') as f:
         js = json.load(f)
         return js
