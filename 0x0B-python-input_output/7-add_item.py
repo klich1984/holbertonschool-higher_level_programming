c91ebc5538b5932ea533a314be08da0792a0848d
klich1984 <2239@holbertonschool.com>
2021-01-20 23:52:01 -0500

    05 | code update | deleted import json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,7 +3,6 @@
 and then save them to a file
 """
 import sys
-import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
