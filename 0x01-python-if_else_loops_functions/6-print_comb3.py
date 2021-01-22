f80a1f7c0f36f38b4d91da0a204eae50855166a9
klich1984 <2239@holbertonschool.com>
2021-01-22 12:36:37 -0500

    00 | add file | code running and output good

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+for i in range(10):
+    for j in range(i + 1, 10):
+        if (i == 8 and j == 9):
+            print("{}{}".format(i, j), end=" ")
+            break
+        print("{}{},".format(i, j), end=" ")
+print("")
