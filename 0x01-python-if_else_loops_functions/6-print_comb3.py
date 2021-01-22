ecf12f04eaa968e02a5d30b9f3123db70467afa2
klich1984 <2239@holbertonschool.com>
2021-01-22 12:41:31 -0500

    00 | add file | erase and space in print end and space in two print

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,5 +5,5 @@ for i in range(10):
         if (i == 8 and j == 9):
             print("{}{}".format(i, j), end=" ")
             break
-        print("{}{},".format(i, j), end=" ")
-print(" ")
+        print("{}{},".format(i, j), end="")
+print("")
