828ed3d2a603e0d80b31fda6333e0a773902d23a
klich1984 <2239@holbertonschool.com>
2021-01-22 12:44:27 -0500

    03 | code update | add space in two print and erase n the 1 print

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if (i == 8 and j == 9):
-            print("{}{}".format(i, j), end=" ")
+            print("{}{}".format(i, j), end="")
             break
-        print("{}{},".format(i, j), end="")
+        print("{}{},".format(i, j), end=" ")
 print("")
