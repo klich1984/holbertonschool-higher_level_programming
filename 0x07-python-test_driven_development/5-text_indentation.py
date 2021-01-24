785bae7bb5cfbe023adeff0f40c6e8e2e9511804
klich1984 <2239@holbertonschool.com>
2021-01-24 12:36:54 -0500

    01 | code update | removing last line break

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -16,5 +16,19 @@ def text_indentation(text):
 # convierte el string texto en una lista por el separador \n
     txt = text.split("\n")
 # imprime la la lista eliminando el espacio ala izquierda
+    i = 0
     for idx in txt:
-        print(idx.lstrip())
+        if i < len(txt) - 1:
+            print(idx.lstrip())
+            i += 1
+    print(idx.lstrip(), end="")
+
+text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+beatiorem! Iam ruinas videres""")
