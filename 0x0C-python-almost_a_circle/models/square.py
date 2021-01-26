f3c781151e37d07492beb75d03c6a80e7da89739
klich1984 <2239@holbertonschool.com>
2021-01-26 01:13:52 -0500

    00 | add file | code running and output good task 9

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""create class Square that inherits from Rectangle"""
+""" create class Square that inherits from Rectangle """
 from models.base import Base
 from models.rectangle import Rectangle
 
