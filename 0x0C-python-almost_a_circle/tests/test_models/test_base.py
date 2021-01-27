4b96e98df9a223c64589fcb39c1ae7b2958bf91a
klich1984 <2239@holbertonschool.com>
2021-01-27 14:15:13 -0500

    06 | test update | test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,42 +1,52 @@
 #!/usr/bin/python3
-"""Test base.py"""
-import pep8
+"""[summary]
+"""
+
 import unittest
 from models import base
 from models.base import Base
+# from models.rectangle import Rectangle
+# from models.square import Square
 
 
-class Test_Base(unittest.TestCase):
-    """create class Base for test program"""
-    def test_pep8_conformance(self):
-        """ Test that pep8 """
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/base.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+class testBase(unittest.TestCase):
 
-    def test_docstrings(self):
-        """ test docstrings """
-        #self.assertIsNotNone(models.base.__doc__)
-        self.assertIsNotNone(Base.__doc__)
-        self.assertIsNotNone(Base.to_json_string.__doc__)
+    def test_doc_class(self):
+
+        self.assertTrue(len(Base.__doc__) > 0)
+
+    def test_doc_file(self):
+
+        self.assertTrue(len(base.__doc__) > 0)
+
+    def test_doc_init(self):
+
+        self.assertTrue(len(Base.__init__.__doc__) > 0)
+
+    def test_doc_to_json_string(self):
+
+        self.assertTrue(len(Base.to_json_string.__doc__) > 0)
+
+    def test_doc_save_to_file(self):
+
+        self.assertTrue(len(Base.save_to_file.__doc__) > 0)
+
+    def test_doc_from_json_string(self):
+
+        self.assertTrue(len(Base.from_json_string.__doc__) > 0)
+
+    def test_doc_create(self):
+
+        self.assertTrue(len(Base.create.__doc__) > 0)
+
+    def test_doc_load_from_file(self):
+
+        self.assertTrue(len(Base.load_from_file.__doc__) > 0)
 
     def test_id(self):
-        """test main"""
-        b1 = Base()
-        self.assertEqual(b1.id, 1)
-        b1 = Base()
-        self.assertEqual(b1.id, 2)
-        b1 = Base()
-        self.assertEqual(b1.id, 3)
-        b1 = Base(12)
-        self.assertEqual(b1.id, 12)
-        b1 = Base()
-        self.assertEqual(b1.id, 4)
-
-    def test_others_cases(self):
-        """other cases"""
-        b1 = Base("klich")
-        self.assertEqual(b1.id, "klich")
-        b1 = Base(-69)
-        self.assertEqual(b1.id, -69)
+        test = Base()
+        self.assertEqual(test.id, 1)
+        test = Base()
+        self.assertEqual(test.id, 2)
+        test = Base(12)
+        self.assertEqual(test.id, 12)
