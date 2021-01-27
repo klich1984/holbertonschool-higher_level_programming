8a60e4cc1ad567f679a17703fe2573e29ec91460
klich1984 <2239@holbertonschool.com>
2021-01-27 14:00:46 -0500

    03 | test update | test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -10,6 +10,13 @@ from models.rectangle import Rectangle
 
 class Test_Base(unittest.TestCase):
     """create class Base for test program"""
+
+    @classmethod
+    def setUpClass(self): 
+        ''' Setting the context for the test'''
+        self.test = Base()
+
+
     def test_pep8_conformance(self):
         """ Test that pep8 """
         pep8style = pep8.StyleGuide(quiet=True)
@@ -23,22 +30,12 @@ class Test_Base(unittest.TestCase):
         self.assertIsNotNone(Base.__doc__)
         self.assertIsNotNone(Base.to_json_string.__doc__)
 
-    def test_main(self):
-        """test main"""
-        b1 = Base()
-        self.assertEqual(b1.id, 1)
-        b2 = Base()
-        self.assertEqual(b2.id, 2)
-        b3 = Base()
-        self.assertEqual(b3.id, 3)
-        b4 = Base(12)
-        self.assertEqual(b4.id, 12)
-        b5 = Base()
-        self.assertEqual(b5.id, 4)
+ 
 
-    def test_others_cases(self):
-        """other cases"""
-        b1 = Base("klich")
-        self.assertEqual(b1.id, "klich")
-        b1 = Base(-69)
-        self.assertEqual(b1.id, -69)
+    def test_base_id(self):
+        '''' Checks instantiation of Base Class'''
+
+        self.assertEqual(self.test.id, 1)
+        self.assertEqual(Base().id, 2)
+        self.assertEqual(Base(50).id, 50)
+
