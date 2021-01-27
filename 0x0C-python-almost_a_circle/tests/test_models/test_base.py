ec0c060c59c42d2827824ecad8f958ca0cf3eb1a
klich1984 <2239@holbertonschool.com>
2021-01-27 00:00:28 -0500

    01 | test update | adding pep8 and docstring

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
 """Test base.py"""
-
-
+import pep8
 import unittest
+import models.base
 from models.base import Base
 
 
 class Test_Base(unittest.TestCase):
     """create class Base for test program"""
+    def test_pep8_conformance(self):
+        """ Test that pep8 """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_docstrings(self):
+        """ test docstrings """
+        self.assertIsNotNone(models.base.__doc__)
+        self.assertIsNotNone(Base.__doc__)
+        self.assertIsNotNone(Base.to_json_string.__doc__)
 
     def test_main(self):
         """test main"""
