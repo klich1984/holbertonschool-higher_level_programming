574810785966f948512a464868eb8b1dccfac9f4
klich1984 <2239@holbertonschool.com>
2021-03-22 16:59:19 -0500

    01 | updatecode | add condition for more arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-if (process.argv[2]) {
+if (process.argv.length === 2) {
   console.log('Argument found');
+} else if (process.argv.length > 2) {
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
