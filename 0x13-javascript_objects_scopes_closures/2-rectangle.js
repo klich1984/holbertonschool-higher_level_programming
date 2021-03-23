18562ef1ed665d876caee714dd55f2f0f26264e5
klich1984 <2239@holbertonschool.com>
2021-03-23 15:33:39 -0500

    01 | update script | script running and good output

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0) {
+    if (w > 0 && h > 0) {
       this.width = w;
-    }
-    if (h > 0 ) {
       this.height = h;
     }
   }
