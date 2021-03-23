350fab6ef34f23fe4751ed7f1f196fcce222de65
klich1984 <2239@holbertonschool.com>
2021-03-23 17:58:55 -0500

    02 | update script | change first conditional

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,11 +4,11 @@ const BeforeSquare = require('./5-square');
 
 module.exports = class Square extends BeforeSquare {
   charPrint (c) {
-    if (c) {
+    if (c != undefined) {
       for (let idx = 0; idx < this.height; idx++) {
         console.log('C'.repeat(this.width));
       }
-    } else if (c === undefined) {
+    } else {
       this.print();
     }
   }
