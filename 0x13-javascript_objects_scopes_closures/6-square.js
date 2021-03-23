ca3673dcae768b4f9d5be630440eb5c3e5b9b6f3
klich1984 <2239@holbertonschool.com>
2021-03-23 17:55:04 -0500

    00 | add script | script running and good output

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const BeforeSquare = require('./5-square');
+
+module.exports = class Square extends BeforeSquare {
+  charPrint (c) {
+    if (c) {
+      for (let idx = 0; idx < this.height; idx++) {
+        console.log('C'.repeat(this.width));
+      }
+    } else {
+      this.print();
+    }
+  }
+};
