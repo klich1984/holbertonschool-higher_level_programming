ae67d05ea48309ac159ad390df69735d09c9a0ab
klich1984 <2239@holbertonschool.com>
2021-03-22 20:08:34 -0500

    00 | add code | code running and good output

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const myVar = process.argv;
+
+const arg2 = parseInt(myVar[2]);
+console.log(arg2);
+
+if ((process.argv.length === 2) || isNaN(arg2)) {
+  console.log('Missing size');
+} else {
+  for (let index = 0; index < arg2; index++) {
+    console.log('x'.repeat(arg2));
+  }
+}
