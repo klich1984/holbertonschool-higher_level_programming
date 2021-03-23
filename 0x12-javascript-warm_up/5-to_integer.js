375b048f09134470f514ced4c1b1258ec19b982c
klich1984 <2239@holbertonschool.com>
2021-03-22 19:29:07 -0500

    01 | update code | code running and good output

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,25 +1,10 @@
 #!/usr/bin/node
 const myVar = process.argv;
-console.log(myVar);
 
-console.log(typeof myVar[2]);
-console.log(typeof myVar[3]);
-console.log(typeof myVar[4]);
+const arg2 = parseInt(myVar[2]);
 
-let arg2 = Number (myVar[2]);
-let arg3 = Number (myVar[3]);
-let arg4 = Number (myVar[4]);
-
-console.log(typeof arg2);
-console.log(typeof arg3);
-console.log(typeof arg4);
-
-console.log(arg2);
-var intValue = parseInt(arg2);
-console.log(intValue);
-
-if ((process.argv.length === 2) || isNaN (arg2)){
-	console.log('Not a number');
+if ((process.argv.length === 2) || isNaN(arg2)) {
+  console.log('Not a number');
 } else {
-	
+  console.log('My number: ' + arg2);
 }
