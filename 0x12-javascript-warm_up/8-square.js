9a3c46ed77b6d2aaf71363e7a2470593e3318ea5
klich1984 <2239@holbertonschool.com>
2021-03-22 20:12:12 -0500

    01 | update code | correcting var x upper case for X

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,12 +2,11 @@
 const myVar = process.argv;
 
 const arg2 = parseInt(myVar[2]);
-console.log(arg2);
 
 if ((process.argv.length === 2) || isNaN(arg2)) {
   console.log('Missing size');
 } else {
   for (let index = 0; index < arg2; index++) {
-    console.log('x'.repeat(arg2));
+    console.log('X'.repeat(arg2));
   }
 }
