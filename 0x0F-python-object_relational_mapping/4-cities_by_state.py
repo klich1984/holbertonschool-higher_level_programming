e6730ff461188d3fed4d7707ff44b929cc81985e
klich1984 <2239@holbertonschool.com>
2021-03-26 16:06:06 -0500

    00 | add script | script running and good output

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+script that lists all cities from the database hbtn_0e_4_usa
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         charset='utf8')
+
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name\
+                 FROM cities JOIN states ON cities.state_id = states.id;")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
