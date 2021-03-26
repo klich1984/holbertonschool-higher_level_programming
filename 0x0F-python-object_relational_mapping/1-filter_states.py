5ba6ef753f714cd766c73e6b481620ec9cfd8681
klich1984 <2239@holbertonschool.com>
2021-03-25 22:15:23 -0500

    01 | add script | corecting style pep8

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa """
+""" script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+"""
 
 import MySQLdb
 from sys import argv
