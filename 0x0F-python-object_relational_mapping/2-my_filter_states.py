9a9060e3fc7498b2e8a4677660480f5013111b53
klich1984 <2239@holbertonschool.com>
2021-03-27 12:06:13 -0500

    03 | update script | change comparation of search for LIKE BINARY

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
                          charset='utf8')
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' \
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
                 ORDER BY id ASC".format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
