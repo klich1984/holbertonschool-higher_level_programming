84b26ce785b8bed8865b19b4b69327a5b3f64731
klich1984 <2239@holbertonschool.com>
2021-03-26 23:11:48 -0500

    00 | add script | script running and good output

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    # generate database schema
+    Base.metadata.create_all(engine)
+    # create a configured "Session" class
+    session = sessionmaker(bind=engine)
+    for state in session().query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
