69c50bdd4c155839dee97bdb0e9d04b94435a3ab
klich1984 <2239@holbertonschool.com>
2021-03-26 23:44:28 -0500

    00 | add script | test - script running and good output

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+script that prints the first State object from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    # generate database schema
+    #Base.metadata.first(engine)
+    # create a configured "Session" class
+    session = sessionmaker(bind=engine)
+    state = session().query(State).first()
+    print("{}: {}".format(state.id, state.name))
+    """for state in session().query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+"""
