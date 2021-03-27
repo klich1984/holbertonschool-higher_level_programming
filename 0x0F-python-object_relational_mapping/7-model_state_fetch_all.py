5ec7258e352bcd78ebcfbfce607da4faa4741c7b
klich1984 <2239@holbertonschool.com>
2021-03-27 00:00:45 -0500

    01 | update  script | add session close

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -20,3 +20,4 @@ if __name__ == "__main__":
     session = sessionmaker(bind=engine)
     for state in session().query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
+    session().close()
