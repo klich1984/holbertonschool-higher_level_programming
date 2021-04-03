33a8ec53241692c90cce27866dd8d081f9745329
klich1984 <2239@holbertonschool.com>
2021-04-02 21:39:17 -0500

    04 | update script | correcting identation

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -13,37 +13,15 @@ from sqlalchemy.orm import relationship
 
 
 if __name__ == "__main__":
-    # create engine
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
-# generate database schema
-Base.metadata.create_all(engine)
-# create a configured "Session" class
-Session = sessionmaker(bind=engine)
-session = Session()
-# addong data Cyty = Father, State = Child
-# add State
-# state_new = State(name="California")
-# session.add(state_new)
-# session.commit()
-# # Add City
-# city_new = City(name="San Francisco", state=state_new)
-# session.add(city_new)
-# session.commit()
-
-# city_new.state_id.append_foreign_key(fk)
-
-# session.add(state_new)
-
-
-# session.add(city_new)
-# # city_new.state = state_new
-# session.commit()
-# session.close()
-new_s = State(name="California")
-new_c = City(name="San Francisco")
-new_s.cities.append(new_c)
-session.add(new_s, new_c)
-session.commit()
-session.close()
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_s = State(name="California")
+    new_c = City(name="San Francisco")
+    new_s.cities.append(new_c)
+    session.add(new_s, new_c)
+    session.commit()
+    session.close()
