cc4e6019661c69d5e98e8b14e62f47f70a112609
klich1984 <2239@holbertonschool.com>
2021-04-02 22:13:15 -0500

    02 | update  script | change imports

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -5,18 +5,18 @@ contained in the database hbtn_0e_101_usa
 """
 
 from sys import argv
-from sqlalchemy import create_engine
+from relationship_city import Base, City
 from relationship_state import Base, State
-from relationship_city import City
-from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    session = Session(engine)
     query = session.query(State).order_by(State.id).all()
     for i in query:
         print(f'{i.id}: {i.name}')
