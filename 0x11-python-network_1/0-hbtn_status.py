6651d09418447f90ab65ca055205215c3a1fef8c
klich1984 <2239@holbertonschool.com>
2021-04-09 14:07:02 -0500

    02 | update script | add if __name__

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,13 @@
 """ script that fetches https://intranet.hbtn.io/status """
 import urllib.request
 
-url = 'https://intranet.hbtn.io/status'
-with urllib.request.urlopen(url) as response:
-    html = response.read()
+if __name__ == '__main__':
 
-print('Body response:')
-print(f'\t- type: {type(html)}')
-print(f'\t- content: {html}')
-print(f"\t- utf8 content: {html.decode('utf-8')}")
+    url = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+
+    print('Body response:')
+    print(f'\t- type: {type(html)}')
+    print(f'\t- content: {html}')
+    print(f"\t- utf8 content: {html.decode('utf-8')}")
