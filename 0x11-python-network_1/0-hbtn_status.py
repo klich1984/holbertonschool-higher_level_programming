c223fb0cd9e2e592500696918c810c1f25df7a28
klich1984 <2239@holbertonschool.com>
2021-04-09 14:08:00 -0500

    03 | update script | identation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     with urllib.request.urlopen(url) as response:
         html = response.read()
 
-    print('Body response:')
-    print(f'\t- type: {type(html)}')
-    print(f'\t- content: {html}')
-    print(f"\t- utf8 content: {html.decode('utf-8')}")
+        print('Body response:')
+        print(f'\t- type: {type(html)}')
+        print(f'\t- content: {html}')
+        print(f"\t- utf8 content: {html.decode('utf-8')}")
