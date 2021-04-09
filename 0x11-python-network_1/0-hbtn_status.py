2ddf20df7eb97efd7dd7a79d07105bff7f97fd8c
klich1984 <2239@holbertonschool.com>
2021-04-09 14:11:35 -0500

    04 | update script | change format print f for .format

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,10 @@ if __name__ == '__main__':
     with urllib.request.urlopen(url) as response:
         html = response.read()
 
-        print('Body response:')
-        print(f'\t- type: {type(html)}')
-        print(f'\t- content: {html}')
-        print(f"\t- utf8 content: {html.decode('utf-8')}")
+    print('Body response:')
+    print('\t- type: {}'.format(type(html)))
+    # print(f'\t- type: {type(html)}')
+    print('\t- content: {}'.format(html))
+    # print(f'\t- content: {html}')
+    print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    # print(f"\t- utf8 content: {html.decode('utf-8')}")
