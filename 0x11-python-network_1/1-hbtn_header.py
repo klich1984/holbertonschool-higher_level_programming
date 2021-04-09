5eca14e9420d3c46d5a544e87ced8ffe1392133a
klich1984 <2239@holbertonschool.com>
2021-04-09 15:16:29 -0500

    00 | add script | test1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""  script that takes in a URL, sends a request to the URL and displays the
+value of the X-Request-Id variable found in the header of the response.
+"""
+import urllib.request
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    url = argv[1]
+    print(url)
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+
+r = response.info()
+print(r)
+print(dir(r))
+print("***"*20)
+print(dir(response))
+print(r.__gt__)
+print("***"*20)
+print(response.getheader('Date'))
