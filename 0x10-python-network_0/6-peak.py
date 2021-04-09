71d3eece8b770b60bec6805794841a1f41592de5
klich1984 <2239@holbertonschool.com>
2021-04-09 10:02:06 -0500

    02 | update file | correcting style pep8

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,6 +10,6 @@ def find_peak(list_of_integers):
     """
     if len(list_of_integers) == 0:
         return None
-    else :
+    else:
         list_of_integers.sort()
         return(list_of_integers[-1])
