e477664e1dc18c93ef35947593c4f1ddd21b40dd
klich1984 <2239@holbertonschool.com>
2021-04-09 00:01:22 -0500

    01 | update script | add all columns in the cut

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL and displays all HTTP methods the server will accept.
-curl -s $1 -I | grep -Fi Allow | cut -d " " -f 2,3,4
+curl -s $1 -I | grep -Fi Allow | cut -d " " -f 2-
