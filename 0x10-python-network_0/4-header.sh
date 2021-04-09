a59274595ea4950b801670be04616e9e7a64fa9b
klich1984 <2239@holbertonschool.com>
2021-04-09 00:11:20 -0500

    01 | update script | erase three line coment

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #  script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-# A header variable X-HolbertonSchool-User-Id must be sent with the value 98
 curl -sH "X-HolbertonSchool-User-Id: 98" $1
