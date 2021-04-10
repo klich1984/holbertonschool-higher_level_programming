5f4c6c985b5d3d9d95de69003fe811f52bd2211a
klich1984 <2239@holbertonschool.com>
2021-04-09 20:21:50 -0500

    00 | add script | good output

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""  script that takes in a URL and an email, sends a POST request to the
+passed URL with the email as a parameter, and displays the body of the
+response (decoded in utf-8)
+"""
+from urllib import request, parse
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    url = argv[1]
+    values = {
+        'email': argv[2]
+    }
+    data = parse.urlencode(values)
+    data = data.encode('utf-8')
+    req = request.Request(url, data)
+    print(req)
+    print(dir(req))
+    with request.urlopen(req) as response:
+        the_page = response.read()
+        print(the_page.decode('utf-8'))
