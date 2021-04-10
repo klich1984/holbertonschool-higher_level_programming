14e2cef57e6f15e8a5d2925bf84e93de69d991bf
klich1984 <2239@holbertonschool.com>
2021-04-09 21:35:53 -0500

    04 | update script | import method error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,7 +2,7 @@
 """ script that takes in a URL, sends a request to the URL and displays
 the body of the response (decoded in utf-8).
 """
-from urllib import request
+from urllib import request, error
 from sys import argv
 
 
