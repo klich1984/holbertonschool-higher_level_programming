c8a529e5c459bce5a317e441544bdbfa6ff55147
klich1984 <2239@holbertonschool.com>
2021-04-09 21:34:53 -0500

    03 | update script | delete import parse

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,7 +2,7 @@
 """ script that takes in a URL, sends a request to the URL and displays
 the body of the response (decoded in utf-8).
 """
-from urllib import request, parse
+from urllib import request
 from sys import argv
 
 
