c84abc7071250c40d9ae39855a1155aeb8d7df66
klich1984 <2239@holbertonschool.com>
2021-04-09 22:44:58 -0500

    01 | update script | add docstring

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
-"""  """
+"""Write a Python script that takes in a URL and an email address, sends a POST
+request to the passed URL with the email as a parameter, and finally displays
+the body of the response.
+
+- The email must be sent in the variable email
+- You must use the packages requests and sys
+- You are not allowed to import packages other than requests and sys
+- You don’t need to error check arguments passed to the script (number or type)
+Please test your script in the container provided, using the web server running
+on port 5000
+"""
 import requests
 from sys import argv
 
