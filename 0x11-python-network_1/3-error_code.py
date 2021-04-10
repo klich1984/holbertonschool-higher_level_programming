1dce61d1d8098219543d22967a22d0d1c215dafb
klich1984 <2239@holbertonschool.com>
2021-04-09 21:24:09 -0500

    01 | update script | add docstring

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""  """
+""" script that takes in a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8).
+"""
 from urllib import request, parse
 from sys import argv
 
