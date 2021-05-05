8a6dc96906b0fa120f93f828175a8a93d669b3e3
klich1984 <2239@holbertonschool.com>
2021-05-04 21:13:38 -0500

    01 | update script | semistandar style

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const myArgs = process.argv;
 const request = require('request');
-url = "https://swapi-api.hbtn.io/api/films/" + myArgs[2]
+const url = 'https://swapi-api.hbtn.io/api/films/' + myArgs[2];
 
 request(url, function (error, response, body) {
   if (error) {
     return console.log(error);
   }
   // console.log('code:', response.statusCode);
-  const info = JSON.parse(body)
-  console.log(info.title)
+  const info = JSON.parse(body);
+  console.log(info.title);
 });
