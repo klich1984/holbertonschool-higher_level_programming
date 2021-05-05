cf8903a944a4df76f1a18ccb5bab7be45ae95b8c
klich1984 <2239@holbertonschool.com>
2021-05-04 22:34:01 -0500

    01 | update script | changue for

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -9,10 +9,17 @@ request(myArgs[2], function (error, response, body) {
   const info = JSON.parse(body);
   let count = 0;
   // console.log(info.results[0].characters);
-  for (let i = 0; i < info.results.length; i++) {
-    if (info.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+  const req = info.results;
+  for (const i of req) {
+    // console.log(i)
+    if (i.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
       count++;
     }
   }
+  // for (let i = 0; i < info.results.length; i++) {
+  //   if (info.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+  //     count++;
+  //   }
+  // }
   console.log(count);
 });
